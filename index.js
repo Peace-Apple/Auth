718e4174570e2cf672ce42695f2b1fb1c5fa5184
peace <astonreba@gmail.com>
2019-08-23 10:03:43 +0300

    Create user model

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-const express = require('express');
+import express from 'express';
+import bodyParser from 'body-parser';
+import authRoute from './routes/auth';
 const port = 3006;
 const app = express()
 
-// import routes
-const authRoute = require('./routes/auth');
+app.use(bodyParser.json());
 
-app.use('api/user', authRoute);
+app.use(
+  bodyParser.urlencoded({
+    limit: '50mb',
+    extended: true
+  })
+);
+
+app.use('/api/user', authRoute);
 
 app.listen(port, () => {console.log(`Server running on port ${port}`)});
--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,31 @@
-const router = require('express').Router();
+import Router from'express';
+import models from '../models';
 
-router.post('/register', (req, res) => {
-    res.send('Register');
+const router = Router();
+
+router.post('/register', async(req, res) => {
+  const { username, email, password } = req.body;
+  try {
+    const user = await models.User.create({
+      username: username,
+      email: email,
+      password: password
+    })
+
+    return res.status(201).json({
+      success: true,
+      message: 'Account successfully created',
+      user: user
+    })
+    
+  } catch (error) {
+    return res.status(400).json(
+      {
+        success: false,
+        message: 'Something went wrong, please try again'
+      }
+    )
+  }
 })
-
-router.post('/login')
 
 module.exports = router;
